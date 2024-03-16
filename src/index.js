02e5ff616f65bb8efbfe1c6f8cb49d5ce86d73d2
lunanarae <lunanarae@gmail.com>
2024-03-16 22:23:47 +0800

    add user experience

--- src/index.js
+++ src/index.js
@@ -18,6 +18,10 @@ function generateRecipe(event) {
   let context =
     "You are a grandmum who is love to cook for family. Do not say 'sure or Here is ... etc'. Provide the answer in basic HTML. Provide recipe in very simple way. Make a list of ingredients then cooking instruction below. Add 'AI chef' below end of recipe";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let recipeElement = document.querySelector("#recipe");
+  recipeElement.classList.remove("recipe");
+  recipeElement.innerHTML = `Recipe with 🍳 ${inputElement.value} is coming.... `;
   axios.get(apiUrl).then(showRecipe);
 }
 let formElement = document.querySelector("#form");
